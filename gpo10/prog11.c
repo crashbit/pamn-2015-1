2133ab82197e9538e007b92928f5f2dedc6d72b9
Germán Santos <crashbito@gmail.com>
2014-08-27 18:51:21 -0500

    Corrección 1

--- gpo10/prog11.c
+++ gpo10/prog11.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+#include <math.h>
+
+double f(double num);
 
 int main(){
-float a, b, c;
+double a, b, c, error;
 int iteracion = 0;
 
 a = 1.2; 
@@ -10,15 +13,39 @@ b = 1.4;
 do{
 	c = (a+b)/2;
 	if(f(a)*f(c)<0)
-		b = C
-	a = c
+		b = c;
+
+	else
+		a = c;
+
+
 
 	printf("\nValor de c %f", c);
 	iteracion = iteracion + 1 ;
 
-}while(iteracion < 5);
+}while(iteracion < 15);
+
+printf("\n\n");
 
 }
+
+double f(double num){
+
+	return (num * exp(num)) - 5;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
