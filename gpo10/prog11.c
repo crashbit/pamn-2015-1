2ecceea41445f683a88fec8ab8d63a6f0a6edc20
Germán Santos <crashbito@gmail.com>
2014-08-27 18:41:38 -0500

    Método de bisección por iteraciones

---
+++ gpo10/prog11.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+float a, b, c;
+int iteracion = 0;
+
+a = 1.2; 
+b = 1.4;
+
+do{
+	c = (a+b)/2;
+	if(f(a)*f(c)<0)
+		b = C
+	a = c
+
+	printf("\nValor de c %f", c);
+	iteracion = iteracion + 1 ;
+
+}while(iteracion < 5);
+
+}
+
+
+
+
+
