e7da4bb90956399b9d9b677b3aebd7a0ad6ea6ed
Germán Santos <crashbito@gmail.com>
2014-08-29 10:10:51 -0500

    Método de bisección

---
+++ gpo15/prog10.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <math.h>
+
+float f(float num);
+
+int main(){
+	
+	float a,b,c, tolerancia, error=0;
+	int i=0;
+	a = 1.2;
+	b = 1.4;
+
+	tolerancia = 0.002;
+
+	do{
+		c = (a+b)/2;
+
+		if(f(a)*f(c)< 0)
+			b=c;
+		else
+			a=c;
+
+		i = i+1;
+
+		error = fabs(a -b);
+
+		printf("\n %d.- %f", i, c);
+
+	}while(error > tolerancia);
+	printf("\n\n");
+
+}
+
+float f(float num){
+
+	return (num * exp(num) - 5);
+}
+
+
+
+
+
+
+
+
+
+
