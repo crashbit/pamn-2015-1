adc268d0b17fa232303bf136dfe7e66c75d09621
Germán Santos <crashbito@gmail.com>
2014-09-05 10:12:21 -0500

    Gauss con sustitución hacia atrás

--- gpo15/prog12.c
+++ gpo15/prog12.c
@@ -6,6 +6,7 @@ int main(){
 	float b[3]={9,24,4};
 	float x[3];
 	int i, j, k, n = 3;
+	float sum;
 
 	float m, a21;
 
@@ -19,12 +20,27 @@ int main(){
 		}
 	}
 
+	// Sustitución hacia atrás
+	x[n-1] = b[n-1]/a[n-1][n-1]; 
+
+	for(i=n-2;i>=0;i--){
+		sum = 0;
+		for(j=i+1;j<n;j++){
+			sum = sum + a[i][j] * x[j];
+		}
+		x[i] = (b[i]-sum)/a[i][i];
+	}
+
 	for(i=0;i<n;i++){
 		for(j=0;j<n;j++){
 			printf("%f ", a[i][j]);
 		}
 		printf("\t| %f", b[i]);
 		printf("\n");
+	}
+
+	for(i=0;i<n;i++){
+		printf("%f\n", x[i]);
 	}
 
 	printf("\n\n");
